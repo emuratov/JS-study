efc564bb7335e25d22f2e5a8b15d16927c02b4dd
emuratov <emuratov@yandex.ru>
2018-01-25 14:32:39 +0700

    1

---
+++ 1.js
@@ -0,0 +1,12 @@
+// Имеется массив произвольной длины, заполненный случайными значениями:
+//     var list = [34, 1, 529, -78, 89.7, 1243];
+
+var list = [34, 1, 529, -78, 89.7, 1243];
+var maxList = list[0];
+
+for (var i = 0; i < list.length; i++){
+    if (maxList < list[i]){
+        maxList = list[i];
+    }
+}
+console.log(maxList);
