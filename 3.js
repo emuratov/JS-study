0d5774ace2ddf9db2203e9fdf8c7395a27bb3c0a
emuratov <emuratov@yandex.ru>
2018-01-27 10:21:36 +0700

    complite

--- 3.js
+++ 3.js
@@ -4,16 +4,16 @@
 
 
 for (var  i = 1; i < 101; i++){
-    if (i%3 === 0 && i%5 != 0){
+    if (i%3 === 0 && i%5 !== 0){
         console.log('Fizz');
     }
-    if (i%5 === 0 && i%3 != 0){
+    if (i%5 === 0 && i%3 !== 0){
         console.log('Buzz');
     }
     if (i%5 === 0 && i%3 === 0){
         console.log('FizzBuzz');
     }
-    if (i%5 != 0 && i%3 != 0){
+    if (i%5 !== 0 && i%3 !== 0){
         console.log(i);
     }
 }
