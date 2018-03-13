6b065cdc97461360052fcc0ade7e32f06505c376
emuratov <emuratov@yandex.ru>
2018-03-13 10:42:41 +0700

    change class by clickable image

---
+++ 1/js.js
@@ -0,0 +1,10 @@
+document.onclick = function (ev) {
+    if (ev.target.tagName == 'IMG') {
+        if (ev.target.classList.value == 'imgTarget') {
+            ev.target.classList.remove('imgTarget');
+        } else
+        if (ev.target.classList.value !== 'imgTarget') {
+            ev.target.classList.add('imgTarget');
+        }
+    }
+}
