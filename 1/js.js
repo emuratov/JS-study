e688005f7080f49c484932cf45dffdbae970f620
emuratov <emuratov@yandex.ru>
2018-03-14 20:54:09 +0700

    complite

--- 1/js.js
+++ 1/js.js
@@ -1,7 +1,7 @@
 // change class by clickable image
 document.onclick = function (ev) {
-    if (ev.target.tagName == 'IMG') {
-        if (ev.target.classList.value == 'imgTarget') {
+    if (ev.target.tagName === 'IMG') {
+        if (ev.target.classList.value === 'imgTarget') {
             ev.target.classList.remove('imgTarget');
         } else
         if (ev.target.classList.value !== 'imgTarget') {
